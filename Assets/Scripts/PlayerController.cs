7da7720543f254efe11ff557fcbe917140d6c071
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-02-17 12:15:44 -0800

    initial commit
    player control/movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
+
 {
+    public float speed = 10;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    horizontalInput = Input.GetAxis("Horizontal");
+    transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
     }
 }
